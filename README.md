eb36e412df01a3b161f9d65eb01a009ee766ff18
ashermorse <ashermorse@icloud.com>
2025-04-14 11:31:56 -0700

    V1.5

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[![My Awesome Stats](https://awesome-github-stats.azurewebsites.net/user-stats/ashermorse?cardType=level-alternate&theme=github-dark&preferLogin=false)](https://git.io/awesome-stats-card)
+![My Awesome Stats](https://awesome-github-stats.azurewebsites.net/user-stats/ashermorse?cardType=level-alternate&theme=github-dark&preferLogin=false)
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src',
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
-  server: {
-    open: true
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  }
-}) 
+}); 
